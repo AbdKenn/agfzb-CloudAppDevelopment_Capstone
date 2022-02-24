ad3d16d11b9944d6fe8e1d8046883a3f97f2695c
AbdKenn <99841144+AbdKenn@users.noreply.github.com>
2022-02-24 10:09:00 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,17 +3,14 @@ import json
 from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
-def get_request(url, api_key = "jTpXaiMwmzYbMR1v9-uJTJb9EPG0gYXte1EQN7hBL9JW", **kwargs):
+def get_request(url,**kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
     try:
-        if api_key:
         # Call get method of requests library with URL and parameters
-            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-                                    auth=HTTPBasicAuth('apikey', api_key))
-        else:
-            response = requests.get(url, headers={'Content-Type': 'application/json'},
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
                                     params=kwargs)
+                                    
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -60,42 +57,17 @@ def get_dealers_from_cf(url, **kwargs):
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
-def get_dealer_reviews_from_cf(url):#, dealerId):
+def get_dealer_reviews_from_cf(url, dealerId):
     results = []
     # Call get_request with a URL parameter
     json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
-        dealers = json_result #["rows"]
+        dealers = json_result["rows"]
     review_obj.sentiment = analyze_review_sentiments(review_obj.review)
-    return review_obj.sentiment
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-def analyze_review_sentiments(text, **kwargs):
-    from ibm_watson import NaturalLanguageUnderstandingV1
-    from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-    from ibm_watson.natural_language_understanding_v1
-    import Features, SentimentOptions
-    #authenticator = IAMAuthenticator(api_key)
-    #natural_language_understanding = NaturalLanguageUnderstandingV1(
-    #    version='2021-08-01',
-    #    authenticator=authenticator
-    #    )
-    #natural_language_understanding.set_service_url('https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/7e025b1b-caf8-45cd-9307-756c56375dee')
-    #response = natural_language_understanding.analyze(
-    #text=text,
-    #features=Features(sentiment=SentimentOptions(targets=['bonds']))).get_result()
-    
-    params = dict()
-    params["text"] = kwargs["text"]
-    params["version"] = kwargs["version"]
-    params["features"] = kwargs["features"]
-    params["return_analyzed_text"] = kwargs["return_analyzed_text"]
 
-    url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/7e025b1b-caf8-45cd-9307-756c56375dee"
-    response = get_request(url, params=params, headers={'Content-Type': 'application/json'},
-                                        auth=HTTPBasicAuth('apikey', "jTpXaiMwmzYbMR1v9-uJTJb9EPG0gYXte1EQN7hBL9JW"))
-    return response
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
 
